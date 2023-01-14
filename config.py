4b868ca38b1c2fb038da6e9ffe335deb54d31456
Konbini98 <118857786+Konbini98@users.noreply.github.com>
2023-01-15 03:49:41 +0630

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", 22681384))
+API_HASH = getenv("API_HASH", "14ae45755537c723aab0564a80d723a9")
+BOT_TOKEN = getenv("BOT_TOKEN", "5833103468:AAF3bnqY-y8Ld44KGskBO43SocsjUR-DDNE")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://autoani:autoani@autoanime.x7uprzw.mongodb.net/?retryWrites=true&w=majority")
 
-INDEX_ID = int(getenv("INDEX_ID"))
-UPLOADS_ID = int(getenv("UPLOADS_ID"))
+INDEX_ID = int(getenv("INDEX_ID", -1001240432549))
+UPLOADS_ID = int(getenv("UPLOADS_ID", -1001876066812))
 
-STATUS_ID = int(getenv("STATUS_ID"))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID"))
+STATUS_ID = int(getenv("STATUS_ID", 3))
+SCHEDULE_ID = int(getenv("SCHEDULE_ID", 4))
 
-CHANNEL_TITLE = getenv("CHANNEL_TITLE")
-INDEX_USERNAME = getenv("INDEX_USERNAME")
-UPLOADS_USERNAME = getenv("UPLOADS_USERNAME")
+CHANNEL_TITLE = getenv("CHANNEL_TITLE", "aeiou")
+INDEX_USERNAME = getenv("INDEX_USERNAME", "mainanimechan")
+UPLOADS_USERNAME = getenv("UPLOADS_USERNAME", "uploadanimechan")
